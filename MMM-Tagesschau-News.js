622106c1dc79e4c0c66800bd4e9c0a486624cecc
HaukeMarquard <120113101+HaukeMarquard@users.noreply.github.com>
2023-12-14 16:12:56 +0100

    .

--- MMM-Tagesschau-News.js
+++ MMM-Tagesschau-News.js
@@ -1,66 +1,19 @@
 Module.register("MMM-Tagesschau-News", {
   defaults: {
-    updateInterval: 10 * 60 * 1000, // every 10 minutes
-    text: "Tomorrow's Weather",
-    lat: "",
-    lon: "",
-    api_key: "",
+    texte: ["Tagesschau", "Tagesthemen", "Nachtmagazin"],
+    index: 0,
   },
   start: function () {
-    Log.info("Starting Module: " + this.name);
-    Log.info("Starting dingens durch hier");
-    this.weather = null;
-    this.textArray = ["Text 1", "Text 2", "Text 3"];
-    this.currentIndex = 0;
-    this.updateDom();
-    this.showNextText();
-  },
-  getStyles: function () {
-    return ["MMM-Tagesschau-News.css"];
-  },
-  getWeather: function () {
-    this.sendSocketNotification("GET_WEATHER", {
-      lat: this.config.lat,
-      lon: this.config.lon,
-      api_key: this.config.api_key,
-    });
-    this.updateDom();
-  },
-  processWeather: function (data) {
-    this.weather = data;
-    //Datenverarbeitung
-    this.updateDom();
+    setInterval(() => {
+      this.config.index = (this.config.index + 1) % this.config.texte.length;
+      this.updateDom();
+    }, 5000);
   },
   getDom: function () {
-    var container = document.createElement("div");
-    container.innerText = this.textArray[this.currentIndex];
-    container.id = "tagesschau_container";
-    return container;
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = `<h2>${this.config.texte[this.config.index]}</h2>`;
+    return wrapper;
   },
-  showNextText: function () {
-    Log.log("showNextText wird aufgerufen");
-
-    if (this.currentIndex < this.textArray.length) {
-      setTimeout(function () {
-        this.currentIndex++;
-        this.updateDom();
-        // Rufe die Funktion erneut auf, nach einer gewissen Zeit (z.B. 5000 ms für 5 Sekunden)
-        setTimeout(this.showNextText, 5000);
-      }, 1000); // 1000 ms für 1 Sekunde Ausblendeffekt
-    }
-  },
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "WEATHER_RESULT") {
-      Log.log("Data kommt an");
-      this.processWeather(payload);
-      this.updateDom();
-    }
-  },
-  sheduleUpdate: function () {
-    var self = this;
-    setInterval(function () {
-      self.getWeather();
-    }, this.config.updateInterval);
-    self.getWeather();
-  },
+});
+  
 });
