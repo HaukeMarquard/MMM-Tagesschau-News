1a9887a12deadfe2e2c66df0d7cf1bb956865017
HaukeMarquard <120113101+HaukeMarquard@users.noreply.github.com>
2023-12-14 15:36:57 +0100

    .

---
+++ MMM-Tagesschau-News.js
@@ -0,0 +1,72 @@
+Module.register("MMM-Tomorrow-Weather", {
+  defaults: {
+    updateInterval: 10 * 60 * 1000, // every 10 minutes
+    text: "Tomorrow's Weather",
+    lat: "",
+    lon: "",
+    api_key: "",
+  },
+  start: function () {
+    Log.info("Starting Module: " + this.name);
+    Log.info("Starting dingens durch hier");
+    this.weather = null;
+    this.textArray = ["Text 1", "Text 2", "Text 3"];
+    this.currentIndex = 0;
+    this.updateDom();
+    this.showNextText();
+  },
+  getStyles: function () {
+    return ["MMM-Tomorrow-Weather.css"];
+  },
+  getWeather: function () {
+    this.sendSocketNotification("GET_WEATHER", {
+      lat: this.config.lat,
+      lon: this.config.lon,
+      api_key: this.config.api_key,
+    });
+    this.updateDom();
+  },
+  processWeather: function (data) {
+    this.weather = data;
+    //Datenverarbeitung
+    this.updateDom();
+  },
+  getDom: function () {
+    var container = document.createElement("div");
+    container.id = "tagesschau_container";
+    return container;
+  },
+  showNextText: function () {
+    let textElement = document.getElementById("tagesschau_container");
+
+    if (this.currentIndex < this.textArray.length) {
+      // Aktuellen Text ausblenden
+      this.textElement.style.opacity = 0;
+
+      setTimeout(function () {
+        // Nächsten Text einblenden
+        this.textElement.innerHTML = this.textArray[this.currentIndex];
+        this.textElement.style.opacity = 1;
+
+        this.currentIndex++;
+
+        // Rufe die Funktion erneut auf, nach einer gewissen Zeit (z.B. 5000 ms für 5 Sekunden)
+        setTimeout(this.showNextText, 5000);
+      }, 1000); // 1000 ms für 1 Sekunde Ausblendeffekt
+    }
+  },
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "WEATHER_RESULT") {
+      Log.log("Data kommt an");
+      this.processWeather(payload);
+      this.updateDom();
+    }
+  },
+  sheduleUpdate: function () {
+    var self = this;
+    setInterval(function () {
+      self.getWeather();
+    }, this.config.updateInterval);
+    self.getWeather();
+  },
+});
---
+++ node_helper.js
@@ -0,0 +1,25 @@
+const NodeHelper = require("node_helper");
+var axios = require("axios");
+const Log = require("logger");
+
+module.exports = NodeHelper.create({
+  start: function () {
+    console.log("Starting node helperrrr: " + this.name);
+    console.log("Hihihi");
+    Log.log("Starting node helperrrriiirrrr: " + this.name);
+  },
+  get_news: function (payload) {
+    var that = this;
+    // this.url = `https://api.tomorrow.io/v4/weather/forecast?location=${payload.lat},${payload.lon}&apikey=${payload.api_key}`
+    this.url = `https://www.tagesschau.de/api2/news/?regions=15`;
+    Log.info(this.url);
+    axios.get(this.url).then((response) => {
+      that.sendSocketNotification("NEWS_RESULT", response.data);
+    });
+  },
+  socketNotificationReceived: function (notification, payload) {
+    if (notification == "GET_NEWS") {
+      this.get_news(payload);
+    }
+  },
+});
